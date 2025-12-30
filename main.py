dd9022d3556c4ec98405592468fe465ddbf92ea5
osman2424 <90058373+osman2424@users.noreply.github.com>
2025-12-30 18:26:49 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,17 @@
+"""
+Ana çalışma dosyası
+Öğrenciler bu dosyayı çalıştıracaktır.
+"""
+
+def main():
+    # TODO:
+    # 1. CSV dosyasını oku
+    # 2. örnekleme frekansını hesapla
+    # 3. temel istatistikleri yazdır
+    # 4. hareketli ortalama filtresi uygula
+    # 5. sonucu çizdir
+    pass
+
+
+if __name__ == "__main__":
+    main()
